b8f23187e3ad5b0749292d33f3f09be6e7cdd0b0
gusta <gustavo.viagem@gmail.com>
2021-09-07 17:57:23 -0300

    add ultrades files to UltraDES folder

---
+++ UltraDES/Data Structures/AdjacencyMatrix.cs
@@ -0,0 +1,177 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+// file:	data structures\adjacencymatrix.cs
+//
+// summary:	Implements the adjacencymatrix class
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltraDES
+{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>   (Serializable)a adjacency matrix. </summary>
+    ///
+    /// <remarks>   Lucas Alves, 18/01/2016. </remarks>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    [Serializable]
+    public class AdjacencyMatrix
+    {
+        /// <summary>   The internal. </summary>
+        private readonly SortedList<int, int>[] m_internal;
+        private readonly BitArray[] m_events;
+        private readonly int m_numerOfEvents;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   Lucas Alves, 18/01/2016. </remarks>
+        ///
+        /// <param name="states">   The states. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public AdjacencyMatrix(int states, int numEvents, bool preAllocate = false)
+        {
+            m_internal = new SortedList<int, int>[states];
+            m_events = new BitArray[states];
+
+            if (preAllocate)
+            {
+                for(var i = 0; i < states; ++i)
+                {
+                    m_internal[i] = new SortedList<int, int>();
+                    m_events[i] = new BitArray(numEvents, false);
+                }
+            }
+            m_numerOfEvents = numEvents;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Indexer to get items within this collection using array index syntax. </summary>
+        ///
+        /// <param name="s">    The int to process. </param>
+        /// <param name="e">    The int to process. </param>
+        ///
+        /// <returns>   The indexed item. </returns>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public int this[int s, int e]
+        {
+            get
+            {
+                return m_events[s][e] ? m_internal[s][e] : -1;
+            }
+        }
+
+        public SortedList<int, int> this[int s]
+        {
+            get
+            {
+                return m_internal[s] ?? (m_internal[s] = new SortedList<int, int>());
+            }
+        }
+
+        public bool hasEvent(int s, int e)
+        {
+            return m_events[s] != null ? m_events[s][e] : false;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets the length. </summary>
+        ///
+        /// <value> The length. </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public int Length
+        {
+            get { return m_internal.Length; }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Adds origin. </summary>
+        ///
+        /// <remarks>   Lucas Alves, 18/01/2016. </remarks>
+        ///
+        /// <param name="origin">   The origin. </param>
+        /// <param name="values">   The values. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void Add(int origin, Tuple<int, int>[] values)
+        {
+            m_internal[origin] = new SortedList<int, int>(values.Length);
+            m_events[origin] = new BitArray(m_numerOfEvents, false);
+
+            foreach (var value in values)
+            {
+                if (!m_events[origin][value.Item1])
+                {
+                    m_internal[origin].Add(value.Item1, value.Item2);
+                    m_events[origin][value.Item1] = true;
+                }
+                else if(m_internal[origin][value.Item1] != value.Item2)
+                {
+                    throw new Exception("Automaton is not deterministic.");
+                }
+            }
+        }
+
+        public void Add(int origin, int e, int dest)
+        {
+            if (m_internal[origin] == null)
+            {
+                m_internal[origin] = new SortedList<int, int>();
+                m_events[origin] = new BitArray(m_numerOfEvents, false);
+            }
+            if (!m_events[origin][e])
+            {
+                m_internal[origin].Add(e, dest);
+                m_events[origin][e] = true;
+            }
+            else if(m_internal[origin][e] != dest)
+            {
+                throw new Exception("Automaton is not deterministic.");
+            }
+        }
+
+        public void Remove(int origin, int e)
+        {
+            m_events[origin][e] = false;
+            m_internal[origin].Remove(e);
+        }
+
+        public AdjacencyMatrix Clone()
+        {
+            var clone = new AdjacencyMatrix(m_internal.Length, m_numerOfEvents);
+
+            for (int i = 0; i < m_internal.Length; i++)
+            {
+                if (m_events[i] != null)
+                    clone.m_events[i] = (BitArray)m_events[i].Clone();
+
+                if (m_internal[i] != null)
+                {
+                    clone.m_internal[i] = new SortedList<int, int>();
+                    foreach (var c in m_internal[i])
+                        clone.m_internal[i].Add(c.Key, c.Value);
+
+                }
+            }
+            return clone;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Trim excess. </summary>
+        ///
+        /// <remarks>   Lucas Alves, 18/01/2016. </remarks>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void TrimExcess()
+        {
+            Parallel.ForEach(m_internal.Where(i => i != null), i => i.TrimExcess());
+        }
+    }
+}
---
+++ UltraDES/States/CompoundState.cs
@@ -0,0 +1,153 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+// file:	States\CompoundState.cs
+//
+// summary:	Implements the compound state class
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Text;
+
+namespace UltraDES
+{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>   (Serializable)a compound state. </summary>
+    /// <remarks>   Lucas Alves, 05/01/2016. </remarks>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    [Serializable]
+    public class CompoundState : AbstractCompoundState
+    {
+        /// <summary>   The hashcode. </summary>
+        private readonly int _hashcode;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Constructor. </summary>
+        /// <remarks>   Lucas Alves, 05/01/2016. </remarks>
+        /// <param name="s1">       The s 1. </param>
+        /// <param name="s2">       The second AbstractState. </param>
+        /// <param name="count">    Number of. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public CompoundState(AbstractState[] s, bool allMarked = true)
+        {
+            S = s;
+            _hashcode = 0;
+            bool marked = allMarked;
+            for (var i = 0; i < S.Length; ++i)
+            {
+                if(allMarked) marked &= S[i].IsMarked;
+                else marked |= S[i].IsMarked;
+                var hash = S[i].GetHashCode();
+                _hashcode = 7 * (_hashcode ^ hash) + 3 * hash;
+            }
+            Marking = marked ? Marking.Marked : Marking.Unmarked;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Constructor. </summary>
+        /// <remarks>   Lucas Alves, 05/01/2016. </remarks>
+        /// <param name="s1">       The s 1. </param>
+        /// <param name="s2">       The second AbstractState. </param>
+        /// <param name="marking">  The marking. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public CompoundState(AbstractState[] s, Marking marking)
+        {
+            S = s;
+            _hashcode = 0;
+            for (var i = 0; i < S.Length; ++i)
+            {
+                var hash = S[i].GetHashCode();
+                _hashcode = 7 * (_hashcode ^ hash) + 3 * hash;
+            }
+            Marking = marking;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets or sets the s 1. </summary>
+        /// <value> The s 1. </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override AbstractState[] S { get; protected set; }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets to marked. </summary>
+        /// <value> to marked. </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override AbstractState ToMarked
+        {
+            get { return IsMarked ? this : new CompoundState(S, Marking.Marked); }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets to unmarked. </summary>
+        /// <value> to unmarked. </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override AbstractState ToUnmarked
+        {
+            get { return !IsMarked ? this : new CompoundState(S, Marking.Unmarked); }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Determines whether the specified object is equal to the current object. </summary>
+        /// <remarks>   Lucas Alves, 05/01/2016. </remarks>
+        /// <param name="obj">  The object to compare with the current object. </param>
+        /// <returns>
+        ///     true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+
+            // If parameter cannot be cast to Point return false.
+            var p = obj as CompoundState;
+            if ((object) p == null) return false;
+
+            if (_hashcode != p._hashcode || S.Length != p.S.Length) return false;
+
+            for (var i = 0; i < S.Length; ++i)
+            {
+                if (S[i] != p.S[i]) return false;
+            }
+
+            return Marking == p.Marking;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Merge with. </summary>
+        /// <remarks>   Lucas Alves, 05/01/2016. </remarks>
+        /// <param name="s2">           The second AbstractState. </param>
+        /// <param name="allMarked">    true if all marked. </param>
+        /// <returns>   A CompoundState. </returns>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override AbstractCompoundState MergeWith(AbstractState s2, bool allMarked = true)
+        {
+            var marked = allMarked ? IsMarked && s2.IsMarked : IsMarked || s2.IsMarked;
+            return new CompoundState(new[] { this, s2 }, marked ? Marking.Marked : Marking.Unmarked);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Serves as the default hash function. </summary>
+        /// <remarks>   Lucas Alves, 05/01/2016. </remarks>
+        /// <returns>   A hash code for the current object. </returns>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override int GetHashCode()
+        {
+            return _hashcode;
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Returns a string that represents the current object. </summary>
+        /// <remarks>   Lucas Alves, 05/01/2016. </remarks>
+        /// <returns>   A string that represents the current object. </returns>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public override string ToString()
+        {
+            var sb = new StringBuilder(S[0].ToString());
+            for (var i = 1; i < S.Length; ++i)
+            {
+                sb.Append("|");
+                sb.Append(S[i].ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
